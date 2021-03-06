6c767cdf93495f38395eeac6f67a4f28195d3e74
Tiago Morais Rosa Luiz <30955027+tiagomrluiz@users.noreply.github.com>
2021-03-05 21:58:34 -0300

    fimDoModuloFunçóes

---
+++ Curso_de_Dart/funcao/map_reduce_1.dart
@@ -0,0 +1,18 @@
+main() {
+  var alunos = [
+    {'nome': 'Alfredo', 'nota': 9.9},
+    {'nome': 'Wilson', 'nota': 9.3},
+    {'nome': 'Mariana', 'nota': 8.7},
+    {'nome': 'Guilherme', 'nota': 8.1},
+    {'nome': 'Ana', 'nota': 7.6},
+    {'nome': 'Ricardo', 'nota': 6.8},
+  ];
+  String Function(Map) pegarApenasONome = (aluno) => aluno['nome'];
+  int Function(String) qtdeDeLetras = (texto) => texto.length;
+  int Function(int) dobroDasLetras = (dobro) => dobro * 2;
+  var resultado = alunos.map(pegarApenasONome)
+  .map(qtdeDeLetras)
+  .map(dobroDasLetras);
+
+  print(resultado);
+}
